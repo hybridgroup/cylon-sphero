0ab3e49a1628c8819517cabd882efbc0a5ad7477
Andrew Stewart <andrew@stwrt.ca>
2014-03-05 13:21:57 -0800

    Remove existing examples, add basic Sphero example

--- examples/basic/sphero.js
+++ examples/basic/sphero.js
@@ -4,9 +4,9 @@ Cylon.robot({
   connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/rfcomm0' },
   device: { name: 'sphero', driver: 'sphero' },
 
-  work: function(self) {
+  work: function(me) {
     every((1).second(), function() {
-      self.sphero.roll(60, Math.floor(Math.random() * 360), 1);
+      me.sphero.roll(60, Math.floor(Math.random() * 360));
     });
   }
 }).start();
