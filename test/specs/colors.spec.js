580ff830780a830bcca8153f8da43a2bcbdbe86b
Andrew Stewart <andrew@stwrt.ca>
2014-06-16 15:19:43 -0700

    Improve specs for Colors module

--- test/specs/colors.spec.js
+++ test/specs/colors.spec.js
@@ -1,23 +1,37 @@
 "use strict";
 
-var Colors = source('colors');
+var colors = source('colors');
 
 describe('Colors', function() {
-  describe('#fromString', function() {
-    it('can fetch hex colors from a string', function() {
-      Colors.fromString('cyan').should.be.equal(0x00FFFF);
-    });
-
-    it('throws an error if a string is not provided', function() {
-      var wrapper = function() { Colors.fromString('ferrarired'); };
-
-      expect(wrapper).to["throw"]();
+  describe("#colors", function() {
+    it("is an object mapping names to hex colors", function() {
+      expect(colors.colors).to.be.an('object');
+      for (var color in colors.colors) {
+        var hex = colors.colors[color];
+        expect(color).to.be.a('string');
+        expect(hex).to.be.a('number');
+      };
     });
   });
 
-  describe('#randomColor', function() {
-    it('can provide random colors', function() {
-      assert(typeof Colors.randomColor() === 'number');
+  describe("#fromString", function() {
+    context("if a color can be found", function() {
+      it("returns the appropriate hex number", function() {
+        expect(colors.fromString("sienna")).to.be.eql(0xA0522D);
+      });
+    });
+
+    context("if a color can't be found", function() {
+      it("throws an error", function() {
+        var fn = function() { return colors.fromString("notacolor"); };
+        expect(fn).to.throw(/No Matching Color/)
+      });
+    });
+  });
+
+  describe("#randomColor", function() {
+    it("returns a random color", function() {
+      expect(colors.randomColor()).to.be.a('number');
     });
   });
 });
